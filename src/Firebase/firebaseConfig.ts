1388acf574ca6e1b9b9ed82d0e329a44670ddc15
Ephraim Park <ephiepark@gmail.com>
2022-10-07 23:29:41 -0700

    initial set up

--- src/Firebase/firebaseConfig.ts
+++ src/Firebase/firebaseConfig.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCo5PX4eewyzK1Ecca_hHLaMuQ3smEKt-w",
-  authDomain: "my-login-app-bc956.firebaseapp.com",
-  projectId: "my-login-app-bc956",
-  storageBucket: "my-login-app-bc956.appspot.com",
-  messagingSenderId: "288290421449",
-  appId: "1:288290421449:web:aefd2bc52a291c0fd22b18",
-  measurementId: "G-RVDTTPMFS0"
+  apiKey: "AIzaSyBMJdzPa4j_9SGbO7XhoIMKJ1zUXwdIIEE",
+  authDomain: "twitter-1-ef070.firebaseapp.com",
+  projectId: "twitter-1-ef070",
+  storageBucket: "twitter-1-ef070.appspot.com",
+  messagingSenderId: "213305537415",
+  appId: "1:213305537415:web:686aadbdcd312764c9d922",
+  measurementId: "G-E23YHD3LRH"
 };
